a5a72acdb331fd9079580d98017e68f20857ab69
JiWoo-Yoo <selfnum_1357@naver.com>
2024-07-18 00:05:56 +0900

    Style: update css

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function showTodo(todoObj) {
   const delBtn = document.createElement("button");
   li.id = todoObj.id;
   li.innerText = todoObj.text;
-  delBtn.innerText = "√";
+  delBtn.innerText = "DEL";
   delBtn.addEventListener("click", deleteTodo);
   li.appendChild(delBtn);
   todoList.appendChild(li);
